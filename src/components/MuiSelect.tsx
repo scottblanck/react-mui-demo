42f83459635f49b236a466573cdc8c746e301164
Scott Blanck <scottblanck@gmail.com>
2022-10-18 16:42:44 -0400

    Format change

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,32 +1,36 @@
-import React from "react";
-import { Box, TextField, MenuItem } from "@mui/material";
-import { useState } from "react";
+import React from 'react'
+import { Box, TextField, MenuItem } from '@mui/material'
+import { useState } from 'react'
 
 export const MuiSelect = () => {
-  const [country, setCountry] = useState("");
-  console.log({ country });
+  const [country, setCountry] = useState('')
+  console.log({ country })
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(event.target.value as string);
-  };
+    setCountry(event.target.value as string)
+  }
+
+  // const handleChangeJSX = (event) => {
+  //   setCountry(event.target.value)
+  // }
 
   return (
     <div>
-      <Box width="250px">
+      <Box width='250px'>
         <TextField
-          label="Select Country"
+          label='Select Country'
           select
           value={country}
           onChange={handleChange}
           fullWidth
           //   can also use same props as TextField for styling, see other component
         >
-          <MenuItem value="IN">India</MenuItem>
-          <MenuItem value="US">USA</MenuItem>
-          <MenuItem value="AU">Austrailia</MenuItem>
+          <MenuItem value='IN'>India</MenuItem>
+          <MenuItem value='US'>USA</MenuItem>
+          <MenuItem value='AU'>Austrailia</MenuItem>
         </TextField>
       </Box>
       <br></br>
     </div>
-  );
-};
+  )
+}
